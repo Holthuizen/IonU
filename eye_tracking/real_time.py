10d99c101b202fdb1db6e4eb3088e46bfc1f1af5
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2021-01-17 19:45:07 +0100

    First commit
    note: removed most test videos and data

---
+++ eye_tracking/real_time.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import dlib
+from gaze_tracking import GazeTracking
+gaze = GazeTracking()
+import csv
+
+#config 
+input_file = "" #path to input video
+filename = "data/test1.csv" #used of data
+debug = False
+frames_of_interest = (10,200)
+cap = cv2.VideoCapture(input_file)
+
+#dlib models
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+#save data 
+data = []
+fcnt = 0
+
+"""
+    relative position of point B between 2 points A and C
+    A < B and B < C
+"""
+def relative_position_between_points(A,B,C):
+    x_delta_AC = C.x - A.x
+    x_delta_AB = B[0] - A.x
+    if x_delta_AC > x_delta_AB: 
+        return x_delta_AB/x_delta_AC
+    else: 
+        print("error false coordinates in frame")
+        return -1
+
+"""logs data to an specified list"""
+def log_eye_data(current_frame_count, looking_direction_left_eye, looking_direction_right_eye, data_log):
+    data_log.append(
+        {'frame':current_frame_count,'L':looking_direction_left_eye, 'R':looking_direction_right_eye}
+    )
+
+''' write log to file'''
+def save_log(filename,dict_data):
+    csv_columns = ['frame','L','R']
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in dict_data:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+
+while True:
+    ret, frame = cap.read()
+    fcnt += 1
+    if not ret or fcnt > frames_of_interest[1]:  
+        print("end of video reached")
+        break
+        #blur = cv2.blur
+
+    if fcnt > frames_of_interest[0]:
+        #find the face in a gray scale version of the frame. 
+        faces = detector(frame)
+        for face in faces:
+            x1 = face.left()
+            y1 = face.top()
+            x2 = face.right()
+            y2 = face.bottom()
+            cv2.rectangle(frame, (x1-10, y1-10), (x2+10, y2+10), (0, 255, 0), 2)
+
+            #find landmarks within the face area
+            landmarks = predictor(frame, face)
+            for n in range(36, 47):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                cv2.circle(frame, (x, y), 2, (255, 0, 0), -1)
+
+            #find landmarks within the face area
+            landmarks = predictor(frame,face)
+            #left eye
+            point_36 = landmarks.part(36)
+            point_39 = landmarks.part(39)
+            #right eye
+            point_42 = landmarks.part(42)
+            point_45 = landmarks.part(45)
+
+            # We send this frame to GazeTracking to analyze it
+            gaze.refresh(frame)
+            left_pupil = gaze.pupil_left_coords()
+            right_pupil = gaze.pupil_right_coords()
+
+            looking_dir_left_eye = relative_position_between_points(point_36,left_pupil,point_39)
+            looking_dir_right_eye = relative_position_between_points(point_42,right_pupil,point_45)
+
+            #collect data 
+            log_eye_data(fcnt, looking_dir_left_eye, looking_dir_right_eye, data)
+
+            
+            
+            if debug:
+                cv2.line(frame,(point_36.x, point_36.y),(point_39.x,point_39.y),(0,200,0),4)
+                cv2.circle(frame, left_pupil, 2, (200, 200, 0), 1)
+                cv2.circle(frame, right_pupil, 2, (200, 200, 0), 1)
+                print(relative_position_between_points(point_36,left_pupil,point_39))
+
+        cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+
+
+# Release everything if job is finished
+cap.release()
+cv2.destroyAllWindows()
+#print(data)
+print("script ended")
+
+#save data
+if input("save data [y/n]:  ")[0].lower() == "y":
+    save_log(filename,data)
+    print(f"saved under {filename}")
---
+++ eye_tracking/resize_with_ffmpeg.py
@@ -0,0 +1,21 @@
+#ffmpeg -i test/video2.mp4 -vf scale=480:320 test/output_320.mp4
+import cv2
+import os
+input_file = ""
+output_file = ""
+dimentions = (400,640)
+#resize command
+command = F"ffmpeg -i {input_file} -vf scale={dimentions[0]}:{dimentions[1]} {output_file}"
+#inspect file
+cap = cv2.VideoCapture(f'{input_file}')
+# characteristics from the original video
+w_frame, h_frame = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#resize video
+if w_frame != dimentions[0] or h_frame != dimentions[1]: 
+    os.system(F'cmd /k "{command}"')
+
+
+
+
+
